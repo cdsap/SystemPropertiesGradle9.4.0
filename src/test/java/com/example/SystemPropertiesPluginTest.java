63cd45f708b770576ff07cd1d86e496b31153b3d
Inaki Villar <inaki.seri@gmail.com>
2026-03-05 16:33:48 -0800

    adding new test

--- src/test/java/com/example/SystemPropertiesPluginTest.java
+++ src/test/java/com/example/SystemPropertiesPluginTest.java
@@ -2,6 +2,10 @@ package com.example;
 
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,11 +16,37 @@ import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SystemPropertiesPluginTest {
 
     @TempDir
     File projectDir;
 
+    @Order(1)
+    @ParameterizedTest
+    @ValueSource(strings = {"9.3.0", "9.3.1", "9.4.0"})
+    void initFile(String gradleVersion) throws IOException {
+        Files.writeString(new File(projectDir, "settings.gradle.kts").toPath(), "");
+        Files.writeString(new File(projectDir, "build.gradle.kts").toPath(),
+            """
+                plugins {
+                    id("com.example.system-properties")
+                }
+                repositories {
+                    mavenCentral()
+                }
+                """);
+
+        GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withPluginClasspath()
+            .withArguments("help", "--configuration-cache")
+            .withGradleVersion(gradleVersion)
+            .build();
+
+    }
+
+    @Order(2)
     @ParameterizedTest
     @ValueSource(strings = {"9.3.0", "9.3.1", "9.4.0"})
     void showSystemPropertiesTaskPrintsProperties(String gradleVersion) throws IOException {
@@ -30,13 +60,6 @@ class SystemPropertiesPluginTest {
                     mavenCentral()
                 }
                 """);
-
-       GradleRunner.create()
-            .withProjectDir(projectDir)
-            .withPluginClasspath()
-            .withArguments("help", "--configuration-cache")
-            .withGradleVersion(gradleVersion)
-            .build();
 
         BuildResult result = GradleRunner.create()
             .withProjectDir(projectDir)
